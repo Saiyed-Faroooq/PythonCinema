ae31aed72205ead85e11c550e8766aef1edd62e6
MovieSaiyedFarooq <testu7760@gmail.com>
2025-12-12 18:00:42 +0000

    new config added

--- movieproject/movieproject/settings.py
+++ movieproject/movieproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-qm^y#$v$#!eq%kcc*#^_uj753zjnyryy128u@uolr_j=&6=-2&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
